6b79862a714e02574f576e91906b88e53b6aa021
Dragoo Cho <deagoocho@gmail.com>
2020-11-21 12:12:46 +0900

    Using Firebase Auth

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,8 +3,7 @@ import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/home";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Switch>
@@ -25,4 +24,5 @@ const AppRouter = () => {
 };
 
 export default AppRouter;
+
 // <>는 Fragment인데 div 같은걸 쓰고싶지 않지만 부모요소가 없을때 많은요소를 렌더링할때 쓴다.
