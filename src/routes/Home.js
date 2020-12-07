2d60d61e45a0bcc9da1ceafdc43479ed55ceb7cf
Dragoo Cho <deagoocho@gmail.com>
2020-12-07 18:20:34 +0900

    #4.2 Uploading

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,7 @@
-import Nweet from "components/Nweet";
-import { dbService } from "fbase";
 import React, { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "fbase";
+import Nweet from "components/Nweet";
 
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
@@ -17,12 +18,15 @@ const Home = ({ userObj }) => {
   }, []);
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection("nweets").add({
-      text: nweet,
-      createdAt: Date.now(),
-      creatorId: userObj.uid,
-    });
-    setNweet("");
+    const fileRef = storageService.ref().child(`${userObj.uid}/${uuidv4()}`);
+    const response = await fileRef.putString(attachment, "data_url");
+    console.log(response);
+    //   await dbService.collection("nweets").add({
+    //     text: nweet,
+    //     createdAt: Date.now(),
+    //     creatorId: userObj.uid,
+    //   });
+    //   setNweet("");
   };
   const onChange = (event) => {
     const {
@@ -36,14 +40,13 @@ const Home = ({ userObj }) => {
     } = event;
     const theFile = files[0];
     const reader = new FileReader();
-    reader.readAsDataURL(theFile);
     reader.onloadend = (finishedEvent) => {
       const {
         currentTarget: { result },
       } = finishedEvent;
       setAttachment(result);
     };
-    // console.log(event.target.files);
+    reader.readAsDataURL(theFile);
   };
   const onClearAttachment = () => setAttachment(null);
   return (
